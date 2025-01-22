cc3720d5f502978b8bd0e871f17b0e4bb34486b4
Carlos Garcia <josecarlosgarciadiaz@gmail.com>
2025-01-21 18:17:09 -0600

    upd

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'gruas.appionic',
-  webDir: 'www'
-};
-
-export default config;
